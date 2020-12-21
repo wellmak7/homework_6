ca6277cba6f78a740ff914180f4b59162ca795eb
Maxim Oglodkov <moglodko@pmintl.net>
2020-12-21 20:00:03 +0300

    +

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-# This is a sample Python script.
+import time
 
+work_time = 12
+
+class TrafficLight:
+    __color = "red"
+    def running(self):
+        for i in range(0, work_time):
+            if i < 7:
+                print("red")
+            elif i < 9:
+                print('yellow')
+            else:
+                print('green')
+            time.sleep()
+
+lights = TrafficLight()
+lights.running()
